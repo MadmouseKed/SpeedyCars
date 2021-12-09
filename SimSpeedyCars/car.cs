38514827ea899f0bc05b38d38b420b7ddc34d1c8
MadmouseKed <75257164+MadmouseKed@users.noreply.github.com>
2021-12-09 15:00:28 +0100

    Road class
    Road met stoplicht mogelijkheid

--- SimSpeedyCars/car.cs
+++ SimSpeedyCars/car.cs
@@ -6,10 +6,10 @@ namespace SpeedyCars
 {
     public class Car
     {
-        float Velocity = 0.0f;
-        float Acceleration = 0.0f;
-        float DeAcceleration = 0.0f;
-        float Length = 0.0f;
+        float Velocity = 0.0f; //Speed car in m/s
+        float Acceleration = 0.0f; //Speed change in m/s2
+        float DeAcceleration = 0.0f; //Speed change in m/s2
+        float Length = 0.0f; //length car in m
 
         public Car(float velocity, float acceleration, float deacceleration, float length)
         {
@@ -43,6 +43,51 @@ namespace SpeedyCars
         public float getLength()
         {
             return Length;
+        }
+    }
+
+    public class Road
+    {
+        float roadLength; //distance in m.
+        List<float> stopLights; //distance from start in m.
+
+        public Road(float length, List<float> lights)
+        {
+            roadLength = length;
+            stopLights = lights;
+        }
+
+        public List<float> getLights()
+        {
+            return stopLights;
+        }
+
+        public float getNextLightDistance(float distance)
+        {
+            if(distance < roadLength)
+            {
+                float pick = 0.0f;
+                bool picked = true;
+                float result;
+                foreach (float lightDistance in stopLights)
+                {
+                    while (picked)
+                    {
+                        if (lightDistance >= distance)
+                        {
+                            pick = lightDistance;
+                            picked = false;
+                        }
+                    }
+                }
+                result = pick - distance;
+                return result;
+
+            }
+            else
+            {
+                throw new Exception("Input distance greater than length of the road.");
+            }
         }
     }
 }
